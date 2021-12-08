0ec604bf870815e3eda0bf2feb0715a82d7ec075
Heeyeon Cho <90899286+heeyeoniii@users.noreply.github.com>
2021-12-08 12:08:43 +0900

    Update 2750_Main.java

--- 정렬/2750_Main.java
+++ 정렬/2750_Main.java
@@ -1,34 +1,36 @@
-import java.util.Arrays; // Arrays.sort()
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// 내가 푼 코드 -> TreeSet 사용
-		Set<Integer> set = new TreeSet<>();
-
-		int n = sc.nextInt();
-
-		for (int i = 0; i < n; i++) {
-			set.add(sc.nextInt());
-		}
-
-		for (Integer i : set) {
-			System.out.println(i);
-		}
-
-		// 버블 정렬
-		// 첫 번째 인덱스부터 시작해 뒤의 인덱스들의 값들과 비교해 나가는 방법
+		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
 
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
 
+		// Arrays.sort() 메소드 -> O(nlongn)
+		Arrays.sort(arr);
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(arr[i]);
+		}
+
+		/*
+		 * Bubble sort -> O(n^2)
+		 * 첫 번째 인덱스부터 시작해 뒤의 인덱스들의 값들과 비교해 최솟값들을
+		 * 차곡차곡 쌓아나가는 방법
+		 * 
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				
 				if (arr[i] > arr[j]) {
 					int temp = arr[j];
 					arr[j] = arr[i];
@@ -36,9 +38,7 @@ public class Main {
 				}
 			}
 		}
+		*/
 		
-		// Arrays.sort()
-		// 자바에서 기본으로 제공되는 메소드, 자동으로 해당 배열이 정렬되어 나온다.
-		Arrays.sort(arr);
 	}
 }
