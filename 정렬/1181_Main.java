686e03de82ec47ae966e71665f4a1cf42c5d7c79
Heeyeon Cho <90899286+heeyeoniii@users.noreply.github.com>
2021-12-09 15:02:40 +0900

    Update 1181_Main.java

--- 정렬/1181_Main.java
+++ 정렬/1181_Main.java
@@ -1,45 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// Arrays.sort() 메소드에 Comparator 구현
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = sc.nextInt();
-		String[] arr = new String[n];
+		int num = Integer.parseInt(br.readLine());
+		String[] arr = new String[num];
 
-		sc.nextLine();
-
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextLine();
+		for (int i = 0; i < num; i++) {
+			arr[i] = br.readLine();
 		}
 
 		Arrays.sort(arr, new Comparator<String>() {
-			
-			// Compare 메소드의 리턴 값 3가지
-			// 양의 정수 : 두 객체의 위치 바꿈
-			// 0 : 변화 X
-			// 음의 정수 : 변화 X
+
 			@Override
 			public int compare(String s1, String s2) {
-				if (s1.length() == s2.length()) { // 단어 길이가 같을 때
-					return s1.compareTo(s2); // 사전 순 정렬
-				} else {
-					return s1.length() - s2.length();
+
+				if (s1.length() == s2.length()) {
+					return s1.compareTo(s2); // 사전순 정렬
 				}
+
+				return s1.length() - s2.length();
 			}
 		});
 
 		System.out.println(arr[0]);
 
-		for (int i = 1; i < n; i++) {
+		for (int i = 1; i < arr.length; i++) {
 			if (!arr[i].equals(arr[i - 1])) {
-				System.out.println(arr[i]); // 단어가 중복될 경우 한 번만 출력
+				System.out.println(arr[i]);
 			}
 		}
+
 	}
 }
