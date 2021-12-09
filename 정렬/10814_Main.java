d1a7a5267db160fefa67d73873783228de3dafee
Heeyeon Cho <90899286+heeyeoniii@users.noreply.github.com>
2021-12-09 15:20:31 +0900

    Update 10814_Main.java

--- 정렬/10814_Main.java
+++ 정렬/10814_Main.java
@@ -1,70 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// 방법 1. Comparator의 compare() 메소드 사용
-		int n = sc.nextInt();
-		String[][] arr = new String[n][2];
+		int num = Integer.parseInt(br.readLine());
+		String[][] arr = new String[num][2];
 
-		for (int i = 0; i < n; i++) {
-			arr[i][0] = sc.next();
-			arr[i][1] = sc.next();
+		StringTokenizer st;
+
+		for (int i = 0; i < num; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			arr[i][0] = st.nextToken();
+			arr[i][1] = st.nextToken();
 		}
 
 		Arrays.sort(arr, new Comparator<String[]>() {
 
 			@Override
 			public int compare(String[] s1, String[] s2) {
-				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
-			}
 
+				if (s1[0] == s2[0]) {
+					return 1;
+
+				} else {
+					return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+				}
+			}
 		});
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < num; i++) {
 			System.out.println(arr[i][0] + " " + arr[i][1]);
-		}
-
-		// 방법 2. 배열을 이용하지 않고 클래스 객체를 만들어 배열처럼 사용
-		Person[] p = new Person[n];
-
-		for (int i = 0; i < n; i++) {
-			p[i] = new Person(sc.nextInt(), sc.next());
-		}
-
-		Arrays.sort(p, new Comparator<Person>() {
-
-			@Override
-			public int compare(Person s1, Person s2) {
-				return s1.age - s2.age;
-			}
-		});
-
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < n; i++) {
-			sb.append(p[i]);
-		}
-		System.out.println(sb);
-	}
-
-	public static class Person {
-		int age;
-		String name;
-
-		public Person(int age, String name) {
-			this.age = age;
-			this.name = name;
-		}
-
-		@Override
-		public String toString() {
-			return age + " " + name + "\n";
 		}
 	}
 }
